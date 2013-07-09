dac1b677bb5d246269c93b472393d0c9e50717e6
John Smith <example@stanford.edu>
2013-07-09 04:55:29 +0000

    Added read from file capability

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var name = "Rajesh Cheethirala";
 app.get('/', function(request, response) {
-  response.send("text");
+  response.send(name);
 });
 
 var port = process.env.PORT || 5000;
