56eab858d6532b604141fa02c02b91d82e8039e2
John Smith <example@stanford.edu>
2013-07-09 04:50:44 +0000

    Added read from file capability

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var text  = buf.toString(fs.readFileSync('index.html','utf8'));
 app.get('/', function(request, response) {
-  response.send(text);
+  response.send("text");
 });
 
 var port = process.env.PORT || 5000;
